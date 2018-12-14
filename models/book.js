817a637bdecf20d6eb114073e82c784d785a9b76
Merwan Guerrib <merwanguerrib@gmail.com>
2018-12-14 12:27:18 +0100

    author.js

--- models/book.js
+++ models/book.js
@@ -1,17 +1,21 @@
 const mongoose = require("mongoose");
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
+var ObjectID = require('mongodb').ObjectID;
 
-const bookSchema = new Schema({
-  title: String,
-  description: String,
-  author: String,
-  rating: Number
-}, {
-  timestamps: {
-    createdAt: "created_at",
-    updatedAt: "updated_at"
+const bookSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    author: [ { type : ObjectID, ref: 'Author' } ],
+    rating: Number
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
 const Book = mongoose.model("Book", bookSchema);
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/book.js");
+const Author = require("../models/author.js")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -67,4 +68,19 @@ router.post('/books/edit', (req, res, next) => {
   })
 });
 
+router.get('/authors/add', (req, res, next) => {
+  res.render("author-add")
+});
+
+router.post('/authors/add', (req, res, next) => {
+  const { name, lastName, nationality, birthday, pictureUrl } = req.body;
+  const newAuthor = new Author({ name, lastName, nationality, birthday, pictureUrl})
+  newAuthor.save()
+  .then((book) => {
+    res.redirect('/books')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+});
 module.exports = router;
